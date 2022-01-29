9b205266493bd47e3cd22a7618aa2554886c841d
marques173 <juliocesar173@hotmail.com>
2022-01-29 16:58:28 -0300

    atualize

---
+++ Aula04/aula06/aula 10/aula 11/ex011.js
@@ -0,0 +1,7 @@
+console.log('console funcionando perfeitamente ')
+var vel = 77.05 // confição simples 
+console.log(`a velociade é ${vel}km por hora`)
+if(vel > 60.5){
+console.log(`vc ultrapassou o limite de velocidade . multado!!`)
+console.log(`Dirija sempre usando sinto de seguraça `)
+}
---
+++ Aula04/aula06/aula 10/aula 11/ex09.js
@@ -0,0 +1,8 @@
+var pais ='EUA'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil'){
+    console.log('voce é brasileiro')
+}else{
+    console.log('vc é brasileiro')
+}
+
